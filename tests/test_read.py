d66cc5efb165094e0146937485077784d0454a9e
Jonathan Kamens <jik@kamens.us>
2023-02-09 11:40:48 -0500

    Don't assume tasks needed for tests exist

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,3 +1,6 @@
+from toodledo import Task
+
+
 def test_get_account(toodledo):
     _ = toodledo.GetAccount()
 
@@ -9,30 +12,52 @@ def test_get_tasks(toodledo):
 
 def test_get_tasks_with_known_folders(toodledo):
     folders = toodledo.GetFolders()
-    tasks = toodledo.GetTasks(params={"fields": "folder"})
-    taskWithPrivateFolder = [t for t in tasks
-                             if t.title == "Test task with private folder"][0]
-    taskWithPublicFolder = [t for t in tasks
-                            if t.title == "Test task with public folder"][0]
     privateFolderId = [f.id_ for f in folders
                        if f.name == "Test Folder - private"][0]
     publicFolderId = [f.id_ for f in folders if f.name == "Test Folder"][0]
 
-    assert taskWithPrivateFolder.folderId == privateFolderId
-    assert taskWithPublicFolder.folderId == publicFolderId
+    wanted_tasks = ([None, "Test task with private folder", privateFolderId],
+                    [None, "Test task with public folder", publicFolderId])
+
+    attempts = 0
+    changed = True
+    while changed and attempts < 2:
+        changed = False
+        attempts += 1
+        tasks = toodledo.GetTasks(params={"fields": "folder"})
+        for wanted in wanted_tasks:
+            try:
+                wanted[0] = next(t for t in tasks if t.title == wanted[1])
+            except StopIteration:
+                toodledo.AddTasks([Task(title=wanted[1], folderId=wanted[2])])
+                changed = True
+
+    for wanted in wanted_tasks:
+        assert wanted[0].folderId == wanted[2]
 
 
 def test_get_tasks_with_known_contexts(toodledo):
     contexts = toodledo.GetContexts()
-    tasks = toodledo.GetTasks(params={"fields": "context"})
-    taskWithPrivateContext = [
-        t for t in tasks if t.title == "Test task with private context"][0]
-    taskWithPublicContext = [
-        t for t in tasks if t.title == "Test task with public context"][0]
     privateContextId = [
         x.id_ for x in contexts if x.name == "Test Context - private"][0]
     publicContextId = [
         x.id_ for x in contexts if x.name == "Test Context - public"][0]
 
-    assert taskWithPrivateContext.contextId == privateContextId
-    assert taskWithPublicContext.contextId == publicContextId
+    wanted_tasks = ([None, "Test task with private context", privateContextId],
+                    [None, "Test task with public context", publicContextId])
+
+    attempts = 0
+    changed = True
+    while changed and attempts < 2:
+        changed = False
+        attempts += 1
+        tasks = toodledo.GetTasks(params={"fields": "context"})
+        for wanted in wanted_tasks:
+            try:
+                wanted[0] = next(t for t in tasks if t.title == wanted[1])
+            except StopIteration:
+                toodledo.AddTasks([Task(title=wanted[1], contextId=wanted[2])])
+                changed = True
+
+    for wanted in wanted_tasks:
+        assert wanted[0].contextId == wanted[2]
