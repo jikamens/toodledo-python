cbd91b5ca40dd9e577149f77d1298a167148ee12
Jonathan Kamens <jik@kamens.us>
2023-02-09 11:01:12 -0500

    Disable authorization test because it's slow and unnecessary
    The authorization test confirms that token refresh happens
    automatically after 100 requests. This is unnecessary because the
    whole test suite uses more than 100 requests so it's guaranteed to
    trip this. This test is slow, so let's just skip it.

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,7 +1,11 @@
 import time
 
-def test_429_error_handling(toodledo):
-	"""Confirm that we automatically refresh our authorization
-	token after 100 requests."""
-	for _ in range(101):
-		_ = toodledo.GetAccount()
+# This test is currently commented out because it takes a long time and it's
+# not necessary because the full test suite is guaranteed to get rate-limited
+# so we don't need a separate test for that.
+
+# def test_429_error_handling(toodledo):
+#       """Confirm that we automatically refresh our authorization
+#       token after 100 requests."""
+#       for _ in range(101):
+#               _ = toodledo.GetAccount()
